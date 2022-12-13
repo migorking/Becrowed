ef91fa808e682fa222138f5761e350e5b1b0ff8a
Mario Igor Barbosa <84343722+migorking@users.noreply.github.com>
2022-12-12 22:14:25 -0300

    Add files via upload

---
+++ 1002_areaCirculo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace areaCirculo_1002
+{
+
+    public class program
+    {
+        public static void Main(string[] args)
+        {
+            double r, area;
+
+            
+            r = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = 3.14159 * (r * r);
+
+
+            Console.WriteLine($"A={area.ToString("f4", CultureInfo.InvariantCulture)}");
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
+
