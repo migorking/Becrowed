e6e669b178c574235ba5aa10f6d5be684efbe8ec
Mario Igor Barbosa <84343722+migorking@users.noreply.github.com>
2022-12-16 19:27:15 -0300

    Add files via upload

---
+++ 1143_QuadradoEaoCubo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace QuadradoEAoCubo_1143 //aqui nome do projeto sempre
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            int entradaN;
+            int elevarAoQuadrado = 0;
+            int elevarAoCubo = 0;
+
+
+            entradaN = int.Parse(Console.ReadLine());
+
+
+
+            for (int i = 1; i <= entradaN; i++)
+            {
+
+                elevarAoQuadrado = i * i;
+                elevarAoCubo = elevarAoQuadrado * i;
+                Console.Write(i + " ");
+                Console.Write(elevarAoQuadrado + " ");
+                Console.WriteLine(elevarAoCubo);
+
+            }
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
